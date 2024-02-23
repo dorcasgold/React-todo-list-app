984c07db96d30ba771b49445e9708fdb12431af0
odetayo dorcas <odetayodorcas123@gmail.com>
2024-02-23 16:50:48 +0100

    clear the form aftes submit

--- src/Components/TodoForm.jsx
+++ src/Components/TodoForm.jsx
@@ -1,15 +1,16 @@
 import { useState } from "react"
 
-function TodoForm() {
+function TodoForm({ addTodo }) {
   const [value, setValue] = useState("")
 
   const handleSubmit = e => {
     e.preventDefault()
-    console.log(value);
+    addTodo(value)
+    setValue("")
   }
   return (
     <form className="TodoForm" onSubmit={handleSubmit}>
-      <input type="text" className="todo-input"
+      <input type="text" className="todo-input" value={value}
         placeholder="Today's Task"
         onChange={(e) => setValue(e.target.value)}
       />
